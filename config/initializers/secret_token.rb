e122498ec547bf4999837a91219b1988fda1419a
Josh Fenio <jfenio@weatheryouwear.com>
2013-07-11 00:15:48 -0600

    Initial Picky commit.  Rails 4 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picky::Application.config.secret_key_base = '249c00f805538f4c5eee1ea3960522ed45a4f2f9ddcc0dcbe9b9646d76c7badb35e2495a9bc7343626d15fde8bc9efccdf234902462052bff7bf2397443f5a20'
