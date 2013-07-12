d29f22b44f52c977f99bceaa6818ac75efdce8d9
Josh Fenio <std3rr@gmail.com>
2013-07-12 00:04:21 -0600

    Add Kaminari, and display players on Dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    @players = Players::Player.all.page(params[:page])
+
+    respond_to do |format|
+      format.html
+    end
   end
 end
