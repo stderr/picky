41ab409664a6aa78a35e6483519f747e01c17c7a
Josh Fenio <std3rr@gmail.com>
2013-07-13 12:01:42 -0600

    WIP

---
+++ app/models/concerns/scoring_systems/scoring_system.rb
@@ -0,0 +1,5 @@
+module ScoringSystems
+  class ScoringSystem
+
+  end
+end
--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -1,6 +1,7 @@
 class Scheduling < ActiveRecord::Base
   belongs_to :player
   belongs_to :game
+  belongs_to :score
 
   validates_presence_of :player, :game
 end
